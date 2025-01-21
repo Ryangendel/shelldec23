7fbe913dc3b3dd7227ca72aa2b7b99e7fd3254e3
Ryan Gendel <ryangendel@Ryans-MacBook-Pro.local>
2025-01-20 20:17:19 -0600

    class 15

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+function HomepageOne(props){
+    console.log(props)
+      return(
+        <div>
+        <div className="homepage">HOMEPAGE</div>
+  
+        </div>
+      )
+    }
+  
+  export default HomepageOne;
